530c9785131a8703d3cdc0b9011f149d48e0f103
AlexanderNP <enin02@mail.ru>
2023-11-17 20:53:13 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,62 @@
+const categoryRows = document.querySelectorAll('.category__row');
+const categoryItem = document.querySelector('.category-seven')
+const categoryAdd = document.querySelector('.category__tab-add')
+
+categoryAdd.addEventListener('click', () => {
+  categoryItem.classList.remove('category-seven')
+  categoryRows.forEach((item) => item.classList.remove('hidden'))
+  categoryAdd.classList.add('hidden')
+})
+
+
+function showCode(el) {
+  const promoCode = 'Cofreak'; // Ваш промокод
+
+  const originalIcon = el.querySelector('.cart__btn-icon');
+  originalIcon.style.display = 'none';
+  
+  const promoText = el.querySelector('.cart__btn-label');
+  promoText.classList = 'cart__btn-label-after-click'
+
+  const newIcon = document.createElement('img');
+  newIcon.className = 'cart__btn-icon';
+  newIcon.src = 'images/main/file-copy.svg';
+  newIcon.alt = 'copy';
+
+  el.insertBefore(newIcon, promoText);
+  promoText.textContent = promoCode;
+
+  el.className = 'cart__promo-btn-after-click';
+
+  el.onclick = function() {
+      navigator.clipboard.writeText(promoCode)
+          .then(() => {
+              alert('Промокод скопирован!');
+          })
+          .catch(err => {
+              alert('Ошибка копирования', err);
+          });
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const showMoreButton = document.querySelector('.brands__btn-more')
+  showLessButton = document.querySelector('.brands__btn-less')
+  const brandList = document.querySelector('.brands__list')
+
+  showMoreButton.addEventListener('click', function() {
+    var scrollHeight = brandList.scrollHeight;
+    brandList.style.maxHeight = scrollHeight + 'px';
+    brandList.classList.add('is-expanded');
+    showMoreButton.classList.add('is-hidden');
+    showLessButton.style.display = 'block'
+  });
+
+  showLessButton.addEventListener('click', function() {
+    brandList.style.maxHeight = '22.04vw';
+    brandList.classList.remove('is-expanded');
+    showMoreButton.classList.remove('is-hidden');
+    showLessButton.style.display = 'none'
+  });
+});
